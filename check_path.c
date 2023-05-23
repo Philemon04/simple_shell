fd1061008641412fd3da7f7b6a0282107710f60c
Philemon04 <sjaden093@gmail.com>
2023-05-23 11:52:22 +0000

    Function to check path

--- check_path.c
+++ check_path.c
@@ -18,3 +18,67 @@ char **search_path(char **environ)
 	 }
 	 return (entry_path);
  }
+
+/**
+ * _strdup - concats strings
+ * @string: the string
+ * Return: the pointer of the concat string
+ */
+
+char *_strdup(char string)
+{
+	int j;
+	int k;
+	char *s = NULL;
+
+	if (string == NULL)
+		return (0);
+	for (j = 0, string[j]; j++)
+		;
+	i++;
+	size  = malloc(j * sizeof(char *));
+	if (!size)
+		return (NULL);
+	for (k = 0; k < j; k++)
+	{
+		size[k] = string[k];
+	}
+	return (s);
+}
+
+/**
+ * _itoa - integer to ascii value
+ * @integer: the integer
+ * @base: the base
+ * Return: char
+ * Reference: https://gist.github.com/narnat/95733cd0ad7bfac0d90697292914c407
+ */
+
+char *_itoa(int integer, int base)
+{
+	static char *array = "0123456789abcdef";
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = integer;
+	
+	if (integer < 0)
+	{
+		n = -integer;
+		sign = '-';
+	}
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do 
+	{
+		*--ptr = array[n % base];
+		n /= base;
+	}
+	while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+		
