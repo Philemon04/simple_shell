cd910824f9d45f4835a9adf1307f7813581020fb
Philemon04 <sjaden093@gmail.com>
2023-05-24 12:23:43 +0000

    Functions to check path

--- check_path.c
+++ check_path.c
@@ -1,26 +1,28 @@
 #include "header.h"
 
-/** search_path - searches for the PATH to execute commands
- *  @environ: Environment variable
- *  Return: the path 
+/**
+ * search_path - searches for the PATH to execute commands
+ * @environ: Environment variable
+ * Return: the path
  */
 
 char **search_path(char **environ)
- {
-	 int p;
-	 char **entry_path;
-	 for (p = 0; environ[p] != NULL; p++)
-	 {
-		 if (environ[p][0] == 'P' && environ[p][2] == 'T')
-		 {
-			 entry_path = _which(environ[p]);
-		 }
-	 }
-	 return (entry_path);
- }
+{
+	int p;
+	char **entry_path;
+	
+	for (p = 0; environ[p] != NULL; p++)
+	{
+		if (environ[p][0] == 'P' && environ[p][2] == 'T')
+		{
+			entry_path = _which(environ[p]);
+		}
+	}
+	return (entry_path);
+}
 
 /**
- * _strdup - concats strings
+ * _stdrup - concats strings
  * @string: the string
  * Return: the pointer of the concat string
  */
@@ -33,11 +35,14 @@ char *_stdrup(char *string)
 
 	if (string == NULL)
 		return (0);
+	
 	for (j = 0; string[j]; j++);
+
 	j++;
 	s = malloc(j * sizeof(char *));
 	if (!s)
 		return (NULL);
+
 	for (k = 0; k < j; k++)
 	{
 		s[k] = string[k];
@@ -69,15 +74,12 @@ char *_itoa(int integer, int base)
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	do 
-	{
+	do {
 		*--ptr = array[n % base];
 		n /= base;
-	}
-	while (n != 0);
+	} while (n != 0);
 
 	if (sign)
 		*--ptr = sign;
 	return (ptr);
 }
-		
