8ff2b8b04310618a42559c5fa68f680c612931a4
Philemon04 <sjaden093@gmail.com>
2023-05-26 20:38:19 +0000

    Shell functions

--- simple_shell.c
+++ simple_shell.c
@@ -3,19 +3,15 @@
 #include <sys/types.h>
 
 /**
- * start_prompt - method prints '$' and gets ready for input
+ * start_prompt - method prints prompt and gets ready for input
  * Return: void
  */
 void start_prompt(void)
 {
-	char *prompt = {"#myShell$ "};
-	char *buffer = getcwd(NULL, 0);
-
 	if (isatty(STDIN_FILENO))
 	{
-		write(STDOUT_FILENO, prompt, _strlen(prompt));
+		write(STDOUT_FILENO, "#myShell$ ", 10);
 	}
-	free(buffer);
 }
 
 /**
@@ -25,38 +21,10 @@ void start_prompt(void)
 char *read_command(void)
 {
 	char *entry = NULL;
-	ssize_t signal;
-	size_t bufsize = 0;
-	int j;
+	ssize_t signal = 0;
 
-	signal = getline(&entry, &bufsize, stdin);
-	if (!entry)
-	{
-		perror("Error when allocating memory for buffer");
-		return (0);
-	}
-	if (signal == 1)
-	{
-		free(entry);
-		return (NULL);
-	}
-	else if (signal == EOF)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "\n", 1);
-		free(entry);
-		exit(0);
-	}
-	else
-	{
-		for (j = 0; entry[j] == ' ' && entry[j + 1] == ' '; j++)
-			;
-		if (!entry[j] && entry[j + 1] == '\n')
-			{
-				free(entry);
-				return (0);
-			}
-	}
+	getline(&entry, &signal, stdin);
+
 	return (entry);
 }
 
@@ -69,38 +37,46 @@ char *read_command(void)
 
 char **get_array_from_str(char *str, char **env)
 {
-	int buffersize = token_Bufsize, position = 0;
+	int i = 0, position = 0;
 	char **entry;
 	char *entry1;
 
-	if (str == NULL)
+	entry = _calloc(1024, sizeof(char *));
+	if (entry == NULL)
 	{
-		return (0);
+		peror("Error");
+		return (NULL);
 	}
-	entry = _calloc(sizeof(char *), buffersize);
-	if (!entry)
-	{
-		perror("Error");
-		exit(EXIT_FAILURE);
-	}
-	entry1 = strtok(str, token_Delim);
+	entry1 = strtok(str, " \n");
 	while (entry1 != NULL)
 	{
-		entry[position] = entry1;
-		position++;
-		entry1 = strtok(NULL, token_Delim);
-	}
-	if (entry[0] == NULL)
-		entry[position] = "\n";
+		if (_strcmp(entry1, "$") == 0)
+		{
+			free(entry[i - 1]);
+			entry[i - 1] = _strdup("env");
+		}
+		else if (_strcmp(entry1, "$?") == 0)
+		{
+			free(entry[i]);
+			entry[i] = _itoa(status);
+		}
+		else if (_strcmp(token, "$$") == 0)
+		{
+			free(entry[i]);
+			entry[i] = _itoa(getpid());
+		}
+		else 
+		{
+			entry[i] = _strdup(entry1);
+			position++;
+		}
 
-	if ((_strcmp(entry[0], "exit") == 0) && entry[1] == NULL)
-	{
-		free(str);
-		free(entry);
-		exit(0);
+		entry1 = strtok(NULL, " \n");
+		i++;
 	}
-	if ((_strcmp(entry[0], "env") == 0) && entry[1] == NULL)
-		print_shell(env);
+
+	entry[position] = NULL;
+
 	return (entry);
 }
 
@@ -113,29 +89,24 @@ char **get_array_from_str(char *str, char **env)
 char **_which(char *foundpath)
 {
 	int size  = token_Bufsize, j = 0;
-	char *copy_path = NULL, *entry =  NULL, *separator = ":=";
-	char **dir = _calloc(sizeof(char *), size);
+	char *copy_path;
+	char **dir = _malloc(size *  sizeof(char *));
 
-	if (foundpath == NULL)
-	{
-		free(foundpath);
-		return (0);
-	}
 	if (dir == NULL)
 	{
-		free(foundpath);
-		perror("Error in allocated memory");
+		perror("Error");
 		return (NULL);
 	}
 
-	copy_path = _strdup(foundpath);
-	entry = strtok(copy_path, separator);
-	while (entry != NULL)
+	copy_path = strtok(foundpath, ":");
+	while (copy_path != NULL)
 	{
-		dir[j] = entry;
+		dir[j] = _strdup(copy_path);
 		j++;
-		entry = strtok(NULL, separator);
+		copy_path = strtok(NULL, ":");
 	}
+
+	dir[j] = NULL:
 
 	return (dir);
 }
@@ -154,32 +125,25 @@ int executable_command(char **av, char **args, char **env, int status_main, int 
 	pid_t pid;
 	int status;
 
-	if (args == NULL)
-		return (-1);
-
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("./hsh: ");
-		exit(1);
+		return;
 	}
 	else if (pid == 0)
 	{
 		if (execve(args[0], args, env) == -1)
 		{
-			_error(av[0], count, args[0]);
+			perror(av[0]);
 			free(args);
-			exit(1);
+			exit(127);
 		}
-		exit(0);
 	}
 	else
 	{
-		if (status_main == 1)
-			free(args[0]);
-
-		free(args);
-		waitpid(pid, &status, WUNTRACED);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status);
 	}
-	return (1);
 }
