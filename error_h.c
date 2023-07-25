c54324f6c766f2703342b74be4de0f06ab37f7bc
Philemon04 <sjaden093@gmail.com>
2023-07-25 17:52:59 +0000

    Handles errors

--- error_h.c
+++ error_h.c
@@ -1,16 +1,17 @@
 #include "header.h"
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 /**
  * get_input - ends the prompt with a new line and handles CTRL + C
  * @input: the input passed
  *
  */
-void get_input(int input __attribute__((unused)))
+void get_input(int input)
 {
-	write(STDOUT_FILENO, "\n#myshell$ ", 11);
+	char *prompt = {"\n#myshell$ "};
+	(void) input;
+
+	write(STDOUT_FILENO, prompt, _strlen(prompt));
+	fflush(stdout);
 }
 
 /**
@@ -18,72 +19,37 @@ void get_input(int input __attribute__((unused)))
  * @cmd: the command pointer
  * @path: the path of the directories
  * Return: 1
- */
+ **/
 int _stat(char **cmd, char **path)
 {
-	char *concat_str;
-	char *divider = "/";
-	int count;
+	char *first_string = NULL, *final_string = NULL;
+	int counter;
 
 	struct stat sb;
 
-	if (cmd[0][0] == '/' || cmd[0][0] == '.')
+	if (path == NULL)
 	{
-		if (stat(cmd[0], &sb) == 0)
-			return (1);
+		free(path);
+		free(cmd);
+		return (0);
 	}
-	else
+
+	for (counter = 0; path[counter] != NULL ; counter++)
 	{
-		for (count = 0; path[count] != NULL; count++)
+		first_string = str_concat(path[counter], "/");
+		final_string = str_concat(first_string, cmd[0]);
+		if (stat(final_string, &sb) == 0 && (sb.st_mode & S_IXUSR))
 		{
-			concat_str = malloc((strlen(path[count]) +
-						strlen(cmd[0]) + 2) * sizeof(char));
-			if (concat_str == NULL)
-			{
-				perror("Error");
-
-				return (0);
-			}
-
-			concat_str = _strcat(_strcpy(concat_str, path[count]), divider);
-			concat_str = _strcat(concat_str, cmd[0]);
-
-			if (stat(concat_str, &sb) == 0)
-			{
-				free(cmd[0]);
-				cmd[0] = _strdup(concat_str);
-				free(concat_str);
-				return (1);
-			}
-
-			free(concat_str);
+			cmd[0] = final_string;																			free(first_string);
+			free(path[0]);
+			free(path);
+			return (1);
 		}
+		free(first_string);
+		free(final_string);
 	}
-
+	free(path[0]);
+	free(path);
 	return (0);
 }
 
-/**
- * strcpy - copy source string to destination
- * @destination - destination
- * @org - origin
- * Return: destination
- **/
-char *_strcpy(char *destination, char *org)
-{
-	char *pointer = destination;
-       
-	while (*pointer)
-		pointer++;
-	
-	while (*org)
-	{
-		*pointer = *org;
-		pointer++;
-		org++;
-	}
-	
-	pointer = '\0';
-	
-	return destination;
-}
